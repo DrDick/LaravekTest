863263c2048468935a57e5d27d9ef669da70a09f
Pavel <57864071+DrDick@users.noreply.github.com>
2023-01-07 12:21:09 +0300

    add blog and menu

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
